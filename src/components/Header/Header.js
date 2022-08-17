93d19c09cff5fe188329ba56640e5e517f93fe8f
Divyanshu Raj <divyanshuraj5050@gmail.com>
2022-08-17 18:45:27 +0530

    add laoding to add post

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,39 @@ import React from "react";
 import ViewAll from "./ViewAll";
 import "./Header.css";
 import { Button } from "antd";
+import LogoutIcon from '@mui/icons-material/Logout';
+import Avatar from "@mui/material/Avatar";
+import { Popconfirm } from 'antd';
 
+import 'antd/dist/antd.css';
+
+// or
+import IconButton from '@mui/material/IconButton';
+
+
+const UserDetails = ({user, logout})=>{
+
+  return (
+
+    <div>
+      <div>
+      Hello, <span style={{maringLeft:"5px", color:"#F29393"}}>{user.name}</span>
+      </div>
+      <div>
+       
+      </div>
+      <div>      
+        <div style={{display:"flex", justifyContent:"flex-end", color:"red", marginTop:"5px"}}>
+          <Button onClick={logout} type="primary" danger>
+           
+            
+          Logout
+          </Button></div>
+
+      </div>
+    </div>
+  )
+}
 const Header = ({logout, user}) => {
   let name = `    ${user.name}`;
   return (
@@ -12,8 +44,29 @@ const Header = ({logout, user}) => {
         <h1 style={{ color:"black"}}>Nobroker Forum</h1>
       </div>
 
-      <div  style={{ position : "absolute" , right : "1rem" ,top : "2.2rem" ,fontSize:"1rem", display:"flex", alignItems:"center", borderRadius:"20px"}}>
-        Hello <span style={{color:"red", fontSize:"1.3rem", marginLeft:"8px"}}>{name}</span>
+      <div  style={{ position : "absolute" , right : "1rem" ,top : "1.2rem" ,fontSize:"1rem", alignItems:"center", borderRadius:"20px"}}>
+       <div style={{display:"flex", alignItems:"baseline",position:"relative"}}>
+       <Popconfirm
+        placement="bottomRight"
+        title={
+       <UserDetails user={user} logout={logout}/>
+        }
+        icon=""
+        okText=""
+        cancelText=""
+        showCancel={false}
+        okButtonProps={{style:{display:"none"}}}
+      >
+        <IconButton>
+       <Avatar alt="Cindy Baker" src={user.image_url} />
+
+       </IconButton>
+      </Popconfirm>
+
+           
+           {/* <span style={{color:"#319DA0", fontSize:"1rem", marginLeft:"8px"}}>{name}</span> */}
+       </div> 
+
       </div>
   
     </div>
